3bfef6be9757fb5c4d99fbce981041d3a97cea84
dan00ile <nikelodeon53@gmail.com>
2025-12-07 23:08:45 +0300

    feat: create abstract class for satellite

---
+++ src/Satellite.java
@@ -0,0 +1,66 @@
+public abstract class Satellite {
+    protected static final int DEFAULT_BATTERY_LEVEL = 100;
+    protected static final boolean DEFAULT_ACTIVE = true;
+
+    private final String name;
+    private boolean isActive;
+    private double batteryLevel;
+
+    public Satellite(String name, boolean isActive, int batteryLevel) {
+        this.name = name;
+        this.isActive = isActive;
+        this.batteryLevel = batteryLevel / 100.0;
+    }
+
+    public boolean activate() {
+        if (isActive) {
+            System.out.println(ConsoleColors.warning(name + ": Уже активирован!"));
+            return false;
+        }
+
+        if (batteryLevel > 0.2) {
+            isActive = true;
+            System.out.println(ConsoleColors.success(name + ": Активация успешна"));
+            return true;
+        }
+
+        System.out.println(ConsoleColors.error(name + ": Ошибка активации (заряд: " +
+                          String.format("%.0f", batteryLevel * 100) + "%)"));
+        return false;
+    }
+    public boolean deactivate() {
+        if (!isActive) {
+            System.out.println(ConsoleColors.warning(name + ": Уже выключен!"));
+            return false;
+        }
+        isActive = false;
+
+        System.out.println(ConsoleColors.success(name + ": Деактивация успешна"));
+        return true;
+    }
+
+    public void consumeBattery(double amount) {
+        if (batteryLevel < amount) {
+            batteryLevel = 0;
+            isActive = false;
+            System.out.println(ConsoleColors.error(name + ": Батарея разряжена. Спутник выключен"));
+            return;
+        }
+        batteryLevel -= amount;
+    }
+
+    protected abstract void performMission();
+
+    protected void printCreationMessage() {
+        System.out.println("Создан спутник: " + this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+}
