c622e9187619474a5286435aba00f8c1a7d34729
dan00ile <nikelodeon53@gmail.com>
2025-12-07 23:22:48 +0300

    feat: make class for Communication Satellite, that extends main abstract class

---
+++ src/CommunicationSatellite.java
@@ -0,0 +1,46 @@
+public class CommunicationSatellite extends Satellite {
+    private final double bandwidth;
+
+    public CommunicationSatellite(String name, boolean isActive, int batteryLevel, double bandwidth) {
+        super(name, isActive, batteryLevel);
+        this.bandwidth = bandwidth;
+        printCreationMessage();
+    }
+
+    public CommunicationSatellite(String name, boolean isActive, double bandwidth) {
+        this(name, isActive, DEFAULT_BATTERY_LEVEL, bandwidth);
+    }
+
+    public CommunicationSatellite(String name, double bandwidth) {
+        this(name, DEFAULT_ACTIVE, DEFAULT_BATTERY_LEVEL, bandwidth);
+    }
+
+    public double getBandwidth() {
+        return bandwidth;
+    }
+
+    @Override
+    protected void performMission() {
+        if (isActive()) {
+            System.out.println(ConsoleColors.colorize(getName() +
+                             ": Передача данных со скоростью " + bandwidth + " Мбит/с",
+                             ConsoleColors.CYAN));
+            sendData(bandwidth);
+            consumeBattery(0.05);
+        } else {
+            System.out.println(ConsoleColors.error(getName() + ": Не может выполнить передачу данных - не активен"));
+        }
+    }
+
+    private void sendData(double data) {
+        System.out.println(ConsoleColors.colorize(getName() +
+                         ": Отправил " + data + " Мбит данных!",
+                         ConsoleColors.GREEN));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + ConsoleColors.colorize(
+               "[Связь: " + bandwidth + " Мбит/с]", ConsoleColors.PURPLE);
+    }
+}
